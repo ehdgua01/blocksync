44e0f1fe2b2ab6118ec1e10105e68a1522cbbe21
KDH <ehdgua01@naver.com>
2020-05-05 19:55:04 +0900

    ADD requirements functions

--- blocksync/file.py
+++ blocksync/file.py
@@ -1,16 +1,17 @@
+import os
 from pathlib import PurePath
-from typing import Tuple, IO, Any, Union, Dict
+from typing import Any, Union, Dict, Callable
 
 import paramiko
 
-from blocksync.consts import SSH_PORT
+from blocksync.consts import SSH_PORT, UNITS
 
 
 class File(object):
     def __init__(
         self,
         path: Union[PurePath, str],
-        mode: str,
+        block_size: int = UNITS["MiB"],
         create: bool = False,
         remote: bool = False,
         hostname: str = None,
@@ -20,17 +21,21 @@ class File(object):
         key_filename: Union[PurePath, str] = "",
         compress: bool = True,
         cipher: str = "aes128-ctr",
-    ):
+    ) -> None:
         __paramiko_ciphers = paramiko.Transport._preferred_ciphers
 
-        if cipher not in __paramiko_ciphers:
-            raise ValueError("Invalid ssh encryption algorithms")
+        if remote and (cipher not in __paramiko_ciphers):
+            raise ValueError("Invalid ssh encryption algorithm")
 
         self._path = path
-        self._mode = mode
+        self._block_size = block_size
+        self._io = None
+        self._size = 0
+
         self._create = create
         self._remote = remote
-        self._remote_options: Dict[str, Any] = {
+        self._ssh = Union[paramiko.SSHClient, None] = None
+        self._ssh_options: Dict[str, Any] = {
             "hostname": hostname,
             "port": port,
             "username": username,
@@ -41,44 +46,66 @@ class File(object):
                 "cipher": [c for c in __paramiko_ciphers if cipher != c]
             },
         }
-        self._ssh = Union[paramiko.SSHClient, None] = None
         self._sftp: Union[paramiko.SFTPClient, None] = None
 
-    def connect(self, session: paramiko.SSHClient = None):
+    def open_sftp(self, session: paramiko.SSHClient = None) -> "File":
         if self.connected:
-            return
+            return self
 
         if not session:
             self._ssh = paramiko.SSHClient()
             self._ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
             self._ssh.load_system_host_keys()
-            self._ssh.connect(**self._remote_options)
+            self._ssh.connect(**self._ssh_options)
         else:
             self._ssh = session
+
         self._sftp = self._ssh.open_sftp()
+        return self
 
-    def close(self):
-        if isinstance(self._ssh, paramiko.SSHClient):
+    def close_sftp(self) -> "File":
+        if isinstance(self._ssh, paramiko.SSHClient) and self.connected:
             self._ssh.close()
+        return self
 
-    def do_create(self, size: int) -> None:
-        if self._remote:
-            if not self.connected:
-                self.connect()
+    def do_create(self, size: int = 0) -> "File":
+        with self._open(self._path, mode="w") as f:
+            f.truncate(size or self._size or 0)
+        return self
 
-            open_ = self._sftp.open
-        else:
-            open_ = open
-
-        with open_(self._path, mode="w") as f:
-            f.truncate(size)
-
-    def do_open(self, path: str, mode: str) -> Tuple[IO, int]:
-        pass
+    def do_open(self) -> "File":
+        self._io = self._open(self._path, mode="rb+")
+        self._io.seek(os.SEEK_SET, os.SEEK_END)
+        self._size = self._io.tell()
+        self._io.seek(os.SEEK_SET)
+        return self
 
     def get_blocks(self) -> Any:
-        pass
+        while self.opened:
+            if block := self._io.read(self._block_size):
+                yield block
+            else:
+                break
+
+    @property
+    def _open(self) -> Callable:
+        if self._remote:
+            if not self.connected:
+                self.open_sftp()
+            return self._sftp.open
+        else:
+            return open
 
     @property
     def connected(self) -> bool:
-        return isinstance(self._sftp, paramiko.SFTPClient)
+        if isinstance(self._ssh, paramiko.SSHClient) and self._ssh.get_transport():
+            return self._ssh.get_transport().is_active()
+        return False
+
+    @property
+    def opened(self) -> bool:
+        try:
+            self._io.tell()
+            return True
+        except:
+            return False
