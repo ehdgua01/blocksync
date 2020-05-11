cd5bd37835502d4aca378d8b202352bfe7093fa0
KDH <ehdgua01@naver.com>
2020-05-11 17:38:26 +0900

    ADD setup.py

--- README.rst
+++ README.rst
@@ -1,5 +1,4 @@
-====
 blocksync
-====
+=========
 
 This repo is a library that you can use [blocksync script](https://github.com/theraser/blocksync) as a library
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="blocksync",
+    version="0.1.2",
+    author="ehdgua01",
+    author_email="ehdgua01@gmail.com",
+    licese="MIT License",
+    description="Synchronize (large) files to a destination (local/remote) using a incremental algorithm",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/ehdgua01/blocksync",
+    platforms="Any",
+    packages=["blocksync"],
+    keywords=["file synchronize", "incremental algorithm"],
+    install_requires=["paramiko"],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
