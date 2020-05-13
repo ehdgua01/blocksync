526e2a8addc8ffdc80fc4ba3e0728ee0adc5b969
KDH <ehdgua01@naver.com>
2020-05-13 11:24:51 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as f:
 
 setuptools.setup(
     name="blocksync",
-    version="0.2.0",
+    version="0.2.1",
     author="ehdgua01",
     author_email="ehdgua01@gmail.com",
     licese="MIT License",
