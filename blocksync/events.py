bd1e6e6a4515c1a0087293fc272bfaf316ebf1a6
KDH <ehdgua01@naver.com>
2021-04-18 00:27:47 +0900

    Modify events

--- blocksync/events.py
+++ blocksync/events.py
@@ -5,25 +5,11 @@ __all__ = ["Events"]
 
 class Events:
     def __init__(self):
-        self.suspend_event: threading.Event = threading.Event()
-        self.canceled: bool = False
+        self.prepared: threading.Event = threading.Event()
+        self.suspended: threading.Event = threading.Event()
+        self.canceled: threading.Event = threading.Event()
 
     def initialize(self):
-        self.suspend_event.set()
-        self.canceled = False
-
-    def cancel(self):
-        self.canceled = True
-
-    def suspend(self):
-        self.suspend_event.clear()
-
-    def resume(self):
-        self.suspend_event.set()
-
-    def wait(self):
-        self.suspend_event.wait()
-
-    @property
-    def suspended(self):
-        return not self.suspend_event.is_set()
+        self.prepared.set()
+        self.suspended.set()
+        self.canceled.set()
