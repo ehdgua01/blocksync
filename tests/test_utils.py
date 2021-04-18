e0f91912a4c0e8385097916260c53e170a6f1bc9
KDH <ehdgua01@naver.com>
2021-04-18 11:42:22 +0900

    Fix utils testcase

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,6 @@
-import unittest
-
 from blocksync.utils import generate_random_data
 
 
-class TestCase(unittest.TestCase):
-    def test_generate_random_data(self) -> None:
-        random_data = generate_random_data(100)
-        self.assertEqual(len(random_data), 100)
-        self.assertTrue(isinstance(random_data, str))
+def test_generate_random_data():
+    random_data = generate_random_data(100)
+    assert len(random_data) == 100
