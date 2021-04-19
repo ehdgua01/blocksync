60e02db35d00d7bf0e4e5487480e85aa11132a60
KDH <ehdgua01@naver.com>
2021-04-19 23:57:27 +0900

    Set block_size to MiB by default

--- blocksync/status.py
+++ blocksync/status.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Literal, TypedDict
 
-BlockTypes = Literal["same", "diff", "done"]
+from blocksync.consts import ByteSizes
 
 
 class Blocks(TypedDict):
@@ -13,12 +13,12 @@ class Blocks(TypedDict):
 class Status:
     def __init__(self):
         self._lock = threading.Lock()
-        self.block_size: int = 0
+        self.block_size: int = ByteSizes.MiB
         self.source_size: int = 0
         self.destination_size: int = 0
         self.blocks: Blocks = Blocks(same=0, diff=0, done=0)
 
-    def initialize(self, /, block_size: int = 0, source_size: int = 0, destination_size: int = 0):
+    def initialize(self, /, block_size: int = ByteSizes.MiB, source_size: int = 0, destination_size: int = 0):
         self.block_size = block_size
         self.source_size = source_size
         self.destination_size = destination_size
@@ -29,9 +29,9 @@ class Status:
             self.blocks[block_type] += 1
             self.blocks["done"] = self.blocks["same"] + self.blocks["diff"]
 
-    def get_rate(self, block_size: int) -> float:
+    def get_rate(self) -> float:
         return (
-            min(100.00, (self.blocks["done"] / (self.source_size // block_size)) * 100)
+            min(100.00, (self.blocks["done"] / (self.source_size // self.block_size)) * 100)
             if self.blocks["done"] > 1
             else 0.00
         )
