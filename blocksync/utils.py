697b82bda41c8866806b81bce1882d44a9490d71
KDH <ehdgua01@naver.com>
2021-04-21 19:13:58 +0900

    Remove redundant module

--- blocksync/utils.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-__all__ = ["generate_random_data"]
-
-
-def generate_random_data(size: int) -> str:
-    return "".join(random.choices(string.ascii_letters + string.digits, k=size))
--- tests/test_utils.py
+++
@@ -1,6 +0,0 @@
-from blocksync.utils import generate_random_data
-
-
-def test_generate_random_data():
-    random_data = generate_random_data(100)
-    assert len(random_data) == 100
