f83600da295521a6c24318181cc249d1e174de95
KDH <ehdgua01@naver.com>
2021-05-06 01:37:00 +0900

    Add read server script

---
+++ blocksync/_read_server.py
@@ -0,0 +1,26 @@
+import base64
+import hashlib
+import io
+import sys
+from typing import Callable
+
+DIFF = b"2"
+COMPLEN = len(DIFF)
+path: bytes = sys.stdin.buffer.readline().strip()
+block_size: int = int(sys.stdin.buffer.readline())
+hash_: Callable = getattr(hashlib, sys.stdin.buffer.readline().strip().decode())
+
+fileobj = open(path, "rb")
+fileobj.seek(io.SEEK_SET, io.SEEK_END)
+print(fileobj.tell(), flush=True)
+
+startpos: int = int(sys.stdin.buffer.readline())
+maxblock: int = int(sys.stdin.buffer.readline())
+
+with fileobj:
+    fileobj.seek(startpos)
+    for _ in range(maxblock):
+        block = fileobj.read(block_size)
+        print(hash_(block).hexdigest(), end="", flush=True)
+        if sys.stdin.buffer.read(COMPLEN) == DIFF:
+            print(base64.b85encode(block).decode(), end="", flush=True)
