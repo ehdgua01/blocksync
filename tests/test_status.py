6fb2965d00e1ac8146f082b893f0405a0e9898dd
KDH <ehdgua01@naver.com>
2021-05-06 23:25:20 +0900

    Fix status test

--- tests/test_status.py
+++ tests/test_status.py
@@ -1,56 +1,48 @@
-from blocksync.consts import ByteSizes
-from blocksync.status import Blocks, Status
+from blocksync._consts import ByteSizes
+from blocksync._status import Blocks
 
 
-def test_initialize_status():
+def test_initialize_status(fake_status):
     # Expect: Initialize all status
-    status = Status()
-    status.initialize(
-        block_size=1_000,
-        source_size=1_000,
-        destination_size=1_000,
-    )
-    status._add_block("same")
-    status._add_block("diff")
-    status.initialize(block_size=10, source_size=10, destination_size=10)
-    assert status.block_size == 10
-    assert status.source_size == 10
-    assert status.destination_size == 10
-    assert status.blocks == Blocks(same=0, diff=0, done=0)
+    assert fake_status.workers == 1
+    assert fake_status.chunk_size == 1_000
+    assert fake_status.block_size == 1_000
+    assert fake_status.src_size == 1_000
+    assert fake_status.dest_size == 1_000
+    assert fake_status.blocks == Blocks(same=0, diff=0, done=0)
 
 
-def test_add_block():
+def test_add_block(fake_status):
     # Expect: Add each blocks and calculate done block
-    status = Status()
-    status._add_block("same")
-    status._add_block("same")
-    status._add_block("diff")
-    assert status.blocks == Blocks(same=2, diff=1, done=3)
+    fake_status.add_block("same")
+    fake_status.add_block("same")
+    fake_status.add_block("diff")
+    assert fake_status.blocks == Blocks(same=2, diff=1, done=3)
 
 
-def test_get_rate():
+def test_get_rate(fake_status):
     # Expect: Return 0.00 when nothing done
-    status = Status()
-    assert status.rate == 0.00
+    assert fake_status.rate == 0.00
 
-    status.initialize(source_size=ByteSizes.MiB * 10, destination_size=ByteSizes.MiB * 10)
+    fake_status.block_size = ByteSizes.MiB
+    fake_status.src_size = fake_status.dest_size = ByteSizes.MiB * 10
 
     # Expect: Return 50.00 when half done
-    status._add_block("same")
-    status._add_block("same")
-    status._add_block("same")
-    status._add_block("diff")
-    status._add_block("diff")
-    assert status.rate == 50.00
+    fake_status.add_block("same")
+    fake_status.add_block("same")
+    fake_status.add_block("same")
+    fake_status.add_block("diff")
+    fake_status.add_block("diff")
+    assert fake_status.rate == 50.00
 
     # Expect: Return 100.00 when all done
-    status._add_block("same")
-    status._add_block("same")
-    status._add_block("same")
-    status._add_block("diff")
-    status._add_block("diff")
-    assert status.rate == 100.00
+    fake_status.add_block("same")
+    fake_status.add_block("same")
+    fake_status.add_block("same")
+    fake_status.add_block("diff")
+    fake_status.add_block("diff")
+    assert fake_status.rate == 100.00
 
     # Expect: Return 100.00 when exceeding the total size
-    status._add_block("diff")
-    assert status.rate == 100.00
+    fake_status.add_block("diff")
+    assert fake_status.rate == 100.00
