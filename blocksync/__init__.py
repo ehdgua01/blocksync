b509c73c460e76b3b3323c8b8d2169cec8a1f126
KDH <ehdgua01@naver.com>
2021-05-18 23:58:39 +0900

    Add SyncManager to __all__

--- blocksync/__init__.py
+++ blocksync/__init__.py
@@ -1,4 +1,5 @@
 from blocksync._status import Status
+from blocksync._sync_manager import SyncManager
 from blocksync.sync import local_to_local, local_to_remote, remote_to_local
 
-__all__ = ["local_to_local", "local_to_remote", "remote_to_local", "Status"]
+__all__ = ["local_to_local", "local_to_remote", "remote_to_local", "Status", "SyncManager"]
